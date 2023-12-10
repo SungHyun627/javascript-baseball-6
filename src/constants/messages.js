b0d1ec2345f124057134cea106135cf597283031
Sunghyun627 <kksscc4@gmail.com>
2023-12-10 22:04:02 +0900

    feat : 스트라이크, 볼 카운트에 따라 출력해야하는 결과를 포매팅해주는 기능 구현

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -9,3 +9,9 @@ export const ERROR_MESSAGES = Object.freeze({
   notThreeDigits: '[ERROR] 주어진 숫자의 개수가 3개가 아닙니다.',
   duplicated: '[ERROR] 중복된 숫자가 있습니다.',
 });
+
+export const RESULT_MESSAGES = Object.freeze({
+  notExistMatchingNumber: '낫싱',
+  matchingAllnumbers: '3개의 숫자를 모두 맞히셨습니다!',
+  gameOver: '게임 종료',
+});
---
+++ src/services/gameResultFormattingService.js
@@ -0,0 +1,15 @@
+import { RESULT_MESSAGES } from '../constants/messages';
+
+class GameResultFormattingService {
+  getGameResultFormat({ strikeCount, ballCount }) {
+    if (strikeCount === 0 && ballCount === 0) {
+      return RESULT_MESSAGES.notExistMatchingNumber;
+    }
+    if (strikeCount === 3) return RESULT_MESSAGES.matchingAllnumbers;
+    if (strikeCount === 0) return `${ballCount} 볼`;
+    if (ballCount === 0) return `${strikeCount} 스트라이크`;
+    return `${ballCount} 볼 ${strikeCount} 스트라이크`;
+  }
+}
+
+export default GameResultFormattingService;
